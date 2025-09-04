e55ccc5be0e6b6cbb102abbb23e13148ed30a0a0
0x584758 <3757549181@qq.com>
2025-09-04 11:20:45 +0800

    初始化项目

---
+++ src/main/java/com/hope/ChuFaLaApplication.java
@@ -0,0 +1,13 @@
+package com.hope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuFaLaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuFaLaApplication.class, args);
+    }
+
+}
